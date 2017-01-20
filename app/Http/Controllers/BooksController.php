c5f32145cac0f1696a5699b664db9a8845466978
Lauro Henrique <henrique_rodrigues_s@yahoo.com.br>
2017-01-20 00:42:47 +0000

    outra correcao da primeira tarefa

--- app/Http/Controllers/BooksController.php
+++ app/Http/Controllers/BooksController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Book;
 use Illuminate\Http\Request;
 
 class BooksController extends Controller
@@ -13,7 +14,9 @@ class BooksController extends Controller
      */
     public function index()
     {
-        //
+        $books = Book::query()->paginate(10);
+
+        return view('books.index', compact('books'));
     }
 
     /**
@@ -23,7 +26,7 @@ class BooksController extends Controller
      */
     public function create()
     {
-        //
+        return view('books/create');
     }
 
     /**
@@ -34,51 +37,66 @@ class BooksController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        Book::create($request->all());
+
+        return redirect()->route('books.index');
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
+     * @param Book $books
      * @return \Illuminate\Http\Response
+     * @internal param int $id
      */
-    public function show($id)
+    public function show(Book $books)
     {
-        //
+        return view('books.show', compact('books'));
     }
 
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  int  $id
+     * @param Request $request
+     * @param Book $book
      * @return \Illuminate\Http\Response
+     * @internal param int $id
      */
-    public function edit($id)
+    public function edit(Request $request, Book $book)
     {
-        //
+        $book->fill($request->all());
+        $book->save();
+
+        return redirect()->route('books.index');
     }
 
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
+     * @param  \Illuminate\Http\Request $request
+     * @param Book $book
      * @return \Illuminate\Http\Response
+     * @internal param int $id
      */
-    public function update(Request $request, $id)
+    public function update(Request $request, Book $book)
     {
-        //
+        $book->fill($request->all());
+        $book->save();
+
+        return redirect()->route('books.index');
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
+     * @param Book $book
      * @return \Illuminate\Http\Response
+     * @internal param int $id
      */
-    public function destroy($id)
+    public function destroy(Book $book)
     {
-        //
+        $book->delete();
+
+        return redirect()->route('books.index');
     }
 }
